65d69c621a83e624e7bd339dd1dbdf449afe2f8c
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-10-15 11:18:08 +0200

    added header

---
+++ web/app/components/Header.tsx
@@ -0,0 +1,10 @@
+import logo from "../Images/knifeEdgeLogo.png"
+const Header = () => {
+  return (
+<header className="header"> <img src={logo} alt="" />
+
+    </header>
+  );
+};
+
+export default Header;
