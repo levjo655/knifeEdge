6dfc97543a1b2950d596bbd1db7b27861c8f2f5f
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-10-15 13:15:52 +0200

    header done

--- web/app/components/Header.tsx
+++ web/app/components/Header.tsx
@@ -1,9 +1,23 @@
-import logo from "../Images/knifeEdgeLogo.png"
+import logo from "../Images/knifeEdgeLogo.png";
 const Header = () => {
   return (
-<header className="header"> <img src={logo} alt="" />
-
-    </header>
+    <div className="flex w-full justify-between bg-slate-300">
+      <img
+        className="w-10 h-auto justify-start"
+        src={logo}
+        alt="knifeEdge logo"
+      />
+      <header className="header justify-between items-end py-4 px-8 ">
+        <nav className="flex space-x-6 justify-end">
+          <ul className="flex space-x-16 justify-end">
+            <li className="hover:text-gray-700 cursor-pointer">Home</li>
+            <li className="hover:text-gray-700 cursor-pointer">Profile</li>
+            <li className="hover:text-gray-700 cursor-pointer">Recepies</li>
+            <li className="hover:text-gray-700 cursor-pointer">About</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 };
 
