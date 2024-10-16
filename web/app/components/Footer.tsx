834c8e6b1753ee945e16330f9c1c627da8a9de2b
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-10-16 15:50:03 +0200

    footer done

---
+++ web/app/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-slate-300 ">
+      <div className="flex justify-center py-4 px-8">
+        <nav className="flex justify-center space-x-6">
+          <ul className="flex  space-x-16">
+            <li className="hover:text-gray-700 cursor-pointer">About</li>
+            <li className="hover:text-gray-700 cursor-pointer">Contact</li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
