29f28638be4c1a586413b2769693f7c8d45b1b76
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-10-17 12:57:12 +0200

    sing up user fixed

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -1,17 +1,114 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type ActionFunctionArgs } from "@remix-run/node";
 import Header from "~/components/Header";
-import IngredientsInput from "~/components/IngredientsInput";
-import UserInputForm from "~/components/userInputForm";
 import Footer from "~/components/Footer";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { getValidatedFormData, useRemixForm } from "remix-hook-form";
+
+const schema = z.object({
+  email: z
+    .string()
+    .email("Invalid email")
+    .min(5, "Email must be at least 5 characters"),
+  password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
+const resolver = zodResolver(schema);
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  const { receivedValues, errors, data } = await getValidatedFormData<
+    z.infer<typeof schema>
+  >(request, resolver);
+  if (errors) {
+    return json({ errors, receivedValues, data });
+  }
+
+  const { email, password } = data;
+
+  const userRes = await fetch("http://localhost:8080/api/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: email,
+      password: password,
+    }),
+  });
+
+  const userData = await userRes.json();
+
+  console.log(userData);
+
+  return null;
+};
 
 export default function Index() {
+  const { formState, handleSubmit, watch, setValue, register } = useRemixForm<
+    z.infer<typeof schema>
+  >({
+    resolver,
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   return (
-    <div className="flex flex-col h-full min-h-screen flex-auto">
+    <div className="flex flex-col h-full min-h-screen">
       <Header />
       <main className="flex-auto justify-center align-middle">
-        <UserInputForm />
-      </main>
+        {/* Form with handleSubmit */}
+        <form
+          className="mx-auto max-w-md p-4"
+          method="post"
+          onSubmit={handleSubmit}
+        >
+          {/* Email input */}
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">
+              Email:
+            </label>
+            <input
+              {...register("email")}
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+            {formState.errors.email && (
+              <p className="text-red-500">{formState.errors.email.message}</p>
+            )}
+          </div>
 
+          {/* Password input */}
+          <div className="mb-4">
+            <label htmlFor="password" className="block mb-2">
+              Password:
+            </label>
+            <input
+              {...register("password")}
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              className="w-full p-2 border border-gray-300 rounded"
+            />
+            {formState.errors.password && (
+              <p className="text-red-500">
+                {formState.errors.password.message}
+              </p>
+            )}
+          </div>
+
+          {/* Submit button */}
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700"
+          >
+            Submit
+          </button>
+        </form>
+      </main>
       <Footer />
     </div>
   );
