a152e2fdb695c840cb786350c2ca07f7cbbada9f
Levjo Cibuku <levjo.cibuku@hotmail.com>
2024-10-29 16:07:56 +0100

    added recipe table

--- web/app/routes/recipe.create/route.tsx
+++ web/app/routes/recipe.create/route.tsx
@@ -1,5 +1,34 @@
+import { json, useLoaderData } from "@remix-run/react";
+import { TypeOf } from "zod";
+import { mongodb } from "~/lib/mongoDb.server";
+import { Link } from "@remix-run/react";
+
+export const loader = async () => {
+  const recipe = await mongodb
+    .db("knifeEdgeRemix")
+    .collection("recipe")
+    .find()
+    .toArray();
+
+  return json({
+    recipe: recipe,
+  });
+};
+
 export const Page = () => {
-  return <div>Recipe</div>;
+  const { recipe } = useLoaderData<typeof loader>();
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black-100">
+      <div className="space-y-4">
+        {recipe.map((x) => (
+          <div className="p-2 bg-blue-500 flex flex-col">
+            <span>{x._id}</span>
+            <span>{x.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Page;
